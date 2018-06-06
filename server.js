6c7a86c70ca2447c838285a4af2cb8d1cc930d1b
Francisco Arroyo <arroyo.franciscojr@gmail.com>
2018-06-06 16:33:52 -0700

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist'));
+app.use(express.static(__dirname + '/dist/ng-todo'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ng-todo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
